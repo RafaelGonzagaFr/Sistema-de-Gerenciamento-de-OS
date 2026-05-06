a8c4c5c3a977bdacb267b34d089f453e70a3d4d6
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2026-05-06 00:32:19 -0300

    Listagem e Cadastro de OS

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,9 @@
 <?php
+require_once "os.php";
+
 session_start();
 
+$ordens = getAllOs();
 // verifica se está logado
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php?erro=nao_autorizado");
@@ -21,6 +24,29 @@ if (!isset($_SESSION['user_id'])) {
     <p>Você está logado no sistema.</p>
 
     <a href="logout.php">Sair</a>
+    <form action="registrarOs.php" method="POST">
+        <input type="text" name="titulo" id="titulo" placeholder="Título">
+        <input type="text" name="descricao" id="descricao" placeholder="Descrição">
+        <input type="submit" value=Registrar nova OS">
+    </form>
 
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Descrição</th>
+            <th>Status</th>
+            <th>Data</th>
+        </tr>
+        <?php foreach ($ordens as $os): ?>
+            <tr>
+                <td><?= $os['id'] ?></td>
+                <td><?= $os['titulo'] ?></td>
+                <td><?= $os['descricao'] ?></td>
+                <td><?= $os['status'] ?></td>
+                <td><?= $os['created_at'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
 </html>
